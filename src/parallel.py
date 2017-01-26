06230d017763625080cb5098080a6494adebbec2
Laurent Gautier <lgautier@gmail.com>
2017-01-26 09:31:01 -0500

    added functionality to parallelize the building of multiples sketches from the same input sequence

--- src/parallel.py
+++ src/parallel.py
@@ -54,3 +54,68 @@ class Sketch(object):
         a.update(b)
         return a
 
+
+class SketchList(object):
+
+    @staticmethod
+    def initializer(clslist, argslist):
+        """
+        FIXME: use of global not really nice (possible root of mysterious issue for user)
+        """
+
+        # Allow automagic expansion of the list of classes
+        if len(clslist) == 1:
+            clslist = tuple(clslist[0] for x in range(len(argslist)))
+
+        # Allow automagic expansion of the list of args
+        if len(argslist) == 1:
+            argslist = tuple(argslist[0] for x in range(len(clslist)))
+        
+        if len(clslist) != len(argslist):
+            raise ValueError("The arguments argslist and clslist must be sequences of either the "
+                             "same length, or of length 1.")
+        
+        global sketchlist_constructor
+        def sketchlist_constructor():
+            return (cls(*args) for cls, args in zip(clslist, argslist))
+
+    
+    @staticmethod
+    def map_sequence(sequence):
+        """
+        - sequence: a bytes-like object
+
+        return: a sketch
+        """
+        mhslist = tuple(sketchlist_constructor())
+        for mhs in mhslist:
+            mhs.add(sequence)
+        return mhslist
+
+    @staticmethod
+    def map_sequences(iterable):
+        """
+        - iterable: an iterable of bytes-like objects
+
+        return: a sketch
+        """
+        mhslist = sketchlist_constructor()
+        for sequence in iterable:
+            for mhs in mhslist:
+                mhs.add(sequence)
+        return mhs
+
+    @staticmethod
+    def reduce(alist, blist):
+        """
+        Update the sketch a with the content of sketch b.
+
+        - alist: a sequence of sketches
+        - blist: a sequence of sketches
+
+        return alist after each of its elements has been updated to the corresponding element in blist
+        """
+        for a,b in zip(alist, blist):
+            a.update(b)
+        return alist
+
--- src/tests/test_parallel.py
+++ src/tests/test_parallel.py
@@ -81,3 +81,110 @@ def test_sketch_reduce_sketches():
     assert mhs.maxsize == mhs_ab.maxsize
     assert mhs.nvisited == mhs_ab.nvisited
     assert len(mhs._heapset ^ mhs_ab._heapset) == 0
+
+
+def test_sketchlist_initializer():
+
+    # empty initializer
+    with pytest.raises(TypeError):
+        mashingpumpkins.parallel.SketchList.initializer()
+
+    nsize = 21
+    maxsize = 10
+    hashfun = lambda input,width,hashbuffer: None
+    seed = 0
+    clslist = (minhashsketch.MaxSketch, minhashsketch.MinSketch)
+
+    # mistmatching lengths
+    with pytest.raises(ValueError):
+        mashingpumpkins.parallel.SketchList.initializer(clslist, [])
+
+    # automagic length adjustment for argslist
+    mashingpumpkins.parallel.SketchList.initializer(clslist, [(nsize, maxsize, hashfun, seed)])
+    hasattr(mashingpumpkins.parallel, 'sketchlist_constructor')
+    l = tuple(mashingpumpkins.parallel.sketchlist_constructor())
+    assert len(l) == len(clslist)
+    for elt, cls in zip(l, clslist):
+        assert type(elt) is cls
+
+    # automagic length adjustment for clslist
+    argslist = [(nsize, maxsize, hashfun, seed),
+                (nsize+1, maxsize, hashfun, seed)]
+    mashingpumpkins.parallel.SketchList.initializer(clslist[:1],
+                                                    argslist)
+    hasattr(mashingpumpkins.parallel, 'sketchlist_constructor')
+    l = tuple(mashingpumpkins.parallel.sketchlist_constructor())
+    assert len(l) == 2
+    for elt, (nsize, maxsize, hashfun, seed) in zip(l, argslist):
+        assert elt.nsize == nsize
+        assert elt.maxsize == maxsize
+        assert elt._hashfun is hashfun
+        assert elt.seed == seed
+
+
+def test_sketchlist_map_sequence():
+
+    nsize = 21
+    maxsize = 10
+    hashfun = hasharray
+    seed = DEFAULT_SEED
+    clslist = (minhashsketch.MaxSketch, minhashsketch.MinSketch)
+    mashingpumpkins.parallel.SketchList.initializer(clslist, [(nsize, maxsize, hashfun, seed)])
+
+    random.seed(123)
+    sequence = b''.join(random.choice((b'A',b'T',b'G',b'C')) for x in range(250))
+    mhslist = mashingpumpkins.parallel.SketchList.map_sequence(sequence)
+
+    for mhs in mhslist:
+        assert mhs.nsize == nsize
+        assert mhs.maxsize == maxsize
+        assert mhs.nvisited == len(sequence)-nsize+1
+
+def test_sketchlist_map_sequences():
+
+    nsize = 21
+    maxsize = 10
+    hashfun = hasharray
+    seed = DEFAULT_SEED
+    cls = minhashsketch.MaxSketch
+    mashingpumpkins.parallel.Sketch.initializer(cls, nsize, maxsize, hashfun, seed)
+
+    random.seed(123)
+    sequence = b''.join(random.choice((b'A',b'T',b'G',b'C')) for x in range(250))
+    sequences = (sequence[beg:end] for beg, end in chunkpos_iter(nsize, len(sequence), 100)) 
+    mhs = mashingpumpkins.parallel.Sketch.map_sequences(sequences)
+
+    assert mhs.nsize == nsize
+    assert mhs.maxsize == maxsize
+    assert mhs.nvisited == len(sequence)-nsize+1
+
+def test_sketchlist_reduce_sketches():
+
+    nsize = 21
+    maxsize = 10
+    hashfun = hasharray
+    seed = DEFAULT_SEED
+    clslist = (minhashsketch.MaxSketch, minhashsketch.MinSketch)
+    mashingpumpkins.parallel.SketchList.initializer(clslist, [(nsize, maxsize, hashfun, seed)])
+
+    mhslist = tuple(cls(nsize, maxsize, hashfun, seed) for cls in clslist)
+    mhslist_a = tuple(cls(nsize, maxsize, hashfun, seed) for cls in clslist)
+    random.seed(123)
+    sequence = b''.join(random.choice((b'A',b'T',b'G',b'C')) for x in range(250))
+    for mhs, mhs_a in zip(mhslist,mhslist_a):
+        mhs.add(sequence)
+        mhs_a.add(sequence)
+
+    mhslist_b = tuple(cls(nsize, maxsize, hashfun, seed) for cls in clslist)
+    random.seed(123)
+    sequence = b''.join(random.choice((b'A',b'T',b'G',b'C')) for x in range(250))
+    for mhs, mhs_b in zip(mhslist, mhslist_b):
+        mhs.add(sequence)
+        mhs_b.add(sequence)
+
+    mhslist_ab = mashingpumpkins.parallel.SketchList.reduce(mhslist_a, mhslist_b)
+    for mhs, mhs_ab in zip(mhslist, mhslist_ab):
+        assert mhs.nsize == mhs_ab.nsize
+        assert mhs.maxsize == mhs_ab.maxsize
+        assert mhs.nvisited == mhs_ab.nvisited
+        assert len(mhs._heapset ^ mhs_ab._heapset) == 0
