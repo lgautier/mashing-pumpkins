73d09bcf39cccce7d5cfde620fbb63a940fd1262
Laurent Gautier <lgautier@gmail.com>
2018-12-21 23:32:47 -0500

    Cleanup / doc.

--- src/sequence.py
+++ src/sequence.py
@@ -7,6 +7,22 @@ def chunkpos_iter(nsize: int, lseq: int, w: int) -> (int, int):
     """
     Iterator of chunk indices.
 
+    This is made to split a long sequence for the purpose of parallel
+    computing on its constituting ngrams/kmers while not using any
+    around the split points.
+
+    For example, a sequence of length 10 for which we want
+    ngrams/kmers of length 3 can be decomposed into the following
+    chunks of length 5 would be split into the following 3 chunks:
+
+    |0 1 2 3 4 5 6 7 8 9|
+     |---------|     :  :
+     :     |---------|  :
+     :     :   : |------|
+     0     :   5 :   :  :
+           3     :   8  :
+                 6      |
+
     - nsize: n in ngram
     - lseq: length of sequence to chunk
     - w: width of window
--- src/tests/test_sequence.py
+++ src/tests/test_sequence.py
@@ -5,8 +5,15 @@ def test_chunkpos_iter():
 
     seq = tuple(range(10))
     nsize = 3
-
     w = 5
+
+    # |0 1 2 3 4 5 6 7 8 9|
+    #  |---------|     :  :
+    #  :     |---------|  :
+    #  :     :   : |------|
+    #  0     :   5 :   :  :
+    #        3     :   8  :
+    #              6      |
     for slice, check in zip(chunkpos_iter(nsize, len(seq), w),
                             ((0, 5), (3, 8), (6, len(seq)))):
         assert slice == check
