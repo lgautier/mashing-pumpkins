c0153e6073b7e8f8b117e629aba2918642822194
Laurent Gautier <lgautier@gmail.com>
2019-12-15 13:28:39 -0500

    Python version.

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Scientific/Engineering",
 ]
 
