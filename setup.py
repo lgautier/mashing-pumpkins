dbd9dd800d8e4763a47550491ec4790d15558695
Laurent Gautier <lgautier@gmail.com>
2019-12-30 13:57:36 -0500

    Force use of setuptools.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import os
 import warnings
 
@@ -32,51 +32,55 @@ if sys.platform == 'darwin':
 elif sys.platform == 'linux':
     pass
 else:
-    raise ValueError("The platform %s is not supported." % sys.platform)
+    warnings.warn("The platform %s is not supported. "
+                  "This may or may not work" % sys.platform)
 
 mmh_mod = Extension("%s._murmurhash3" % PACKAGENAME,
                     sources=["src/_murmurhash3.cpp", "src/MurmurHash3.cpp"],
                     depends=["src/MurmurHash3.h"],
                     include_dirs=["src",],
                     language="c++",
-                    extra_compile_args = extra_compile_args + \
-                    ['-O3'])                    
+                    extra_compile_args=(extra_compile_args+
+                                        ['-O3']))
 
 mmhmash_mod = Extension("%s._murmurhash3_mash" % PACKAGENAME,
-                        sources=["src/_murmurhash3_mash.cpp", "src/MurmurHash3.cpp"],
+                        sources=["src/_murmurhash3_mash.cpp",
+                                 "src/MurmurHash3.cpp"],
                         depends=["src/MurmurHash3.h"],
                         include_dirs=["src",],
                         language="c++",
-                        extra_compile_args = extra_compile_args + \
-                        ['-O3'])                    
+                        extra_compile_args=(extra_compile_args+
+                                            ['-O3']))
 
 xxh_mod = Extension("%s._xxhash" % PACKAGENAME,
                     sources=["src/_xxhash.c", "src/xxhash.c"],
                     depends=["src/xxhash.h"],
                     include_dirs=["src",],
                     language="c",
-                    extra_compile_args = extra_compile_args + \
-                    ['-O3',
-                     '-std=c99',
-                     '-Wall', '-Wextra', '-Wcast-qual', '-Wcast-align', '-Wshadow',
-                     '-Wstrict-aliasing=1', '-Wswitch-enum', '-Wdeclaration-after-statement',
-                     '-Wstrict-prototypes', '-Wundef'])
+                    extra_compile_args = (
+                        extra_compile_args +
+                        ['-O3',
+                         '-std=c99',
+                         '-Wall', '-Wextra', '-Wcast-qual',
+                         '-Wcast-align', '-Wshadow',
+                         '-Wstrict-aliasing=1', '-Wswitch-enum',
+                         '-Wdeclaration-after-statement',
+                         '-Wstrict-prototypes', '-Wundef'])
+                    )
 
 setup(
-    name = PYPINAME,
-    version = VERSION,
-    description = "Hash sketches of sequences",
-    license = "MIT",
-    author = "Laurent Gautier",
-    author_email = "lgautier@gmail.com",
-    url = "https://github.com/lgautier/mashing-pumpkins",
-    packages = [PACKAGENAME,
-                PACKAGENAME + '.tests'],
-    package_dir = {PACKAGENAME: 'src'},
-    ext_modules = [mmh_mod, mmhmash_mod, xxh_mod],
-    extras_require = {
-        'test' : ['pytest']},
-    classifiers = CLASSIFIERS)
-
-
-
+    name=PYPINAME,
+    version=VERSION,
+    description="Hash sketches of sequences",
+    license="MIT",
+    author="Laurent Gautier",
+    author_email="lgautier@gmail.com",
+    url="https://github.com/lgautier/mashing-pumpkins",
+    packages=[PACKAGENAME,
+              PACKAGENAME + '.tests'],
+    package_dir={PACKAGENAME: 'src'},
+    ext_modules=[mmh_mod, mmhmash_mod, xxh_mod],
+    requires=['setuptools'],
+    extras_require={
+        'test': ['pytest']},
+    classifiers=CLASSIFIERS)
